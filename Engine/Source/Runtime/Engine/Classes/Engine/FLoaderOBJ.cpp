285a9cb1f487dde0cd7fc4aa6db0c61a864c9a5a
Hellowhales <hellowhales7@gmail.com>
2025-04-01 03:31:50 +0900

    Insurance

--- Engine/Source/Runtime/Engine/Classes/Engine/FLoaderOBJ.cpp
+++ Engine/Source/Runtime/Engine/Classes/Engine/FLoaderOBJ.cpp
@@ -3,6 +3,83 @@
 #include "Components/Material/Material.h"
 #include "Components/Mesh/StaticMesh.h"
 
+OBJ::FStaticMeshRenderData* FManagerOBJ::LoadObjStaticMeshAsset(const FString& PathFileName)
+{
+    OBJ::FStaticMeshRenderData* NewStaticMesh = new OBJ::FStaticMeshRenderData();
+        
+    if ( const auto It = ObjStaticMeshMap.Find(PathFileName))
+    {
+        return *It;
+    }
+        
+    FWString BinaryPath = (PathFileName + ".bin").ToWideString();
+    if (std::ifstream(BinaryPath).good())
+    {/*
+            if (LoadStaticMeshFromBinary(BinaryPath, *NewStaticMesh))
+            {
+                ObjStaticMeshMap.Add(PathFileName, NewStaticMesh);
+                return NewStaticMesh;
+            }*/
+    }
+        
+    // Parse OBJ
+    FObjInfo NewObjInfo;
+    bool Result = FLoaderOBJ::ParseOBJ(PathFileName, NewObjInfo);
+    QEMSimplifier::Simplify(NewObjInfo, NewObjInfo.Vertices.Num() * 0.45);
+    NewObjInfo.MaterialSubsets[0].IndexCount = NewObjInfo.VertexIndices.Num();
+    if (!Result)
+    {
+        delete NewStaticMesh;
+        return nullptr;
+    }
+
+    // Material
+    if (NewObjInfo.MaterialSubsets.Num() > 0)
+    {
+        Result = FLoaderOBJ::ParseMaterial(NewObjInfo, *NewStaticMesh);
+
+        if (!Result)
+        {
+            delete NewStaticMesh;
+            return nullptr;
+        }
+
+        CombineMaterialIndex(*NewStaticMesh);
+
+        for (int materialIndex = 0; materialIndex < NewStaticMesh->Materials.Num(); materialIndex++) {
+            CreateMaterial(NewStaticMesh->Materials[materialIndex]);
+        }
+    }
+        
+    // Convert FStaticMeshRenderData
+    Result = FLoaderOBJ::ConvertToStaticMesh(NewObjInfo, *NewStaticMesh);
+    if (!Result)
+    {
+        delete NewStaticMesh;
+        return nullptr;
+    }
+
+    SaveStaticMeshToBinary(BinaryPath, *NewStaticMesh);
+    ObjStaticMeshMap.Add(PathFileName, NewStaticMesh);
+    return NewStaticMesh;
+}
+
+void FManagerOBJ::CombineMaterialIndex(OBJ::FStaticMeshRenderData& OutFStaticMesh)
+{
+    for (int32 i = 0; i < OutFStaticMesh.MaterialSubsets.Num(); i++)
+    {
+        FString MatName = OutFStaticMesh.MaterialSubsets[i].MaterialName;
+        for (int32 j = 0; j < OutFStaticMesh.Materials.Num(); j++)
+        {
+            if (OutFStaticMesh.Materials[j].MTLName == MatName)
+            {
+                OutFStaticMesh.MaterialSubsets[i].MaterialIndex = j;
+                break;
+            }
+        }
+    }
+}
+
 UMaterial* FManagerOBJ::CreateMaterial(FObjMaterialInfo materialInfo)
 {
     if (materialMap[materialInfo.MTLName] != nullptr)
--- Engine/Source/Runtime/Engine/Classes/Engine/FLoaderOBJ.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/FLoaderOBJ.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <fstream>
+#include <queue>
+#include <set>
 #include <sstream>
 
 #include "Define.h"
@@ -431,81 +433,9 @@ struct FLoaderOBJ
 struct FManagerOBJ
 {
 public:
-    static OBJ::FStaticMeshRenderData* LoadObjStaticMeshAsset(const FString& PathFileName)
-    {
-        OBJ::FStaticMeshRenderData* NewStaticMesh = new OBJ::FStaticMeshRenderData();
-        
-        if ( const auto It = ObjStaticMeshMap.Find(PathFileName))
-        {
-            return *It;
-        }
-        
-        FWString BinaryPath = (PathFileName + ".bin").ToWideString();
-        if (std::ifstream(BinaryPath).good())
-        {/*
-            if (LoadStaticMeshFromBinary(BinaryPath, *NewStaticMesh))
-            {
-                ObjStaticMeshMap.Add(PathFileName, NewStaticMesh);
-                return NewStaticMesh;
-            }*/
-        }
-        
-        // Parse OBJ
-        FObjInfo NewObjInfo;
-        bool Result = FLoaderOBJ::ParseOBJ(PathFileName, NewObjInfo);
+    static OBJ::FStaticMeshRenderData* LoadObjStaticMeshAsset(const FString& PathFileName);
 
-        if (!Result)
-        {
-            delete NewStaticMesh;
-            return nullptr;
-        }
-
-        // Material
-        if (NewObjInfo.MaterialSubsets.Num() > 0)
-        {
-            Result = FLoaderOBJ::ParseMaterial(NewObjInfo, *NewStaticMesh);
-
-            if (!Result)
-            {
-                delete NewStaticMesh;
-                return nullptr;
-            }
-
-            CombineMaterialIndex(*NewStaticMesh);
-
-            for (int materialIndex = 0; materialIndex < NewStaticMesh->Materials.Num(); materialIndex++) {
-                CreateMaterial(NewStaticMesh->Materials[materialIndex]);
-            }
-        }
-        
-        // Convert FStaticMeshRenderData
-        Result = FLoaderOBJ::ConvertToStaticMesh(NewObjInfo, *NewStaticMesh);
-        if (!Result)
-        {
-            delete NewStaticMesh;
-            return nullptr;
-        }
-
-        SaveStaticMeshToBinary(BinaryPath, *NewStaticMesh);
-        ObjStaticMeshMap.Add(PathFileName, NewStaticMesh);
-        return NewStaticMesh;
-    }
-    
-    static void CombineMaterialIndex(OBJ::FStaticMeshRenderData& OutFStaticMesh)
-    {
-        for (int32 i = 0; i < OutFStaticMesh.MaterialSubsets.Num(); i++)
-        {
-            FString MatName = OutFStaticMesh.MaterialSubsets[i].MaterialName;
-            for (int32 j = 0; j < OutFStaticMesh.Materials.Num(); j++)
-            {
-                if (OutFStaticMesh.Materials[j].MTLName == MatName)
-                {
-                    OutFStaticMesh.MaterialSubsets[i].MaterialIndex = j;
-                    break;
-                }
-            }
-        }
-    }
+    static void CombineMaterialIndex(OBJ::FStaticMeshRenderData& OutFStaticMesh);
 
     static bool SaveStaticMeshToBinary(const FWString& FilePath, const OBJ::FStaticMeshRenderData& StaticMesh)
     {
@@ -711,4 +641,300 @@ private:
     inline static TMap<FString, OBJ::FStaticMeshRenderData*> ObjStaticMeshMap;
     inline static TMap<FWString, UStaticMesh*> staticMeshMap;
     inline static TMap<FString, UMaterial*> materialMap;
+};
+// Quadric 에러 행렬 (대칭행렬의 10개 요소만 저장)
+struct Quadric {
+    float data[10] = {0};
+
+    void Add(const Quadric& q) {
+        for (int i = 0; i < 10; i++) {
+            data[i] += q.data[i];
+        }
+    }
+};
+
+// 엣지 축소 후보 구조체 (operator< 정의로 우선순위 큐에서 낮은 cost 우선)
+struct IndexSet
+{
+    IndexSet(uint32 v, uint32 n, uint32 t) :verIndex(v), norIndex(n), texIndex(t) {}
+    uint32 verIndex, norIndex, texIndex;
+    bool operator<(const IndexSet& other) const {
+            return verIndex < other.verIndex;
+    }
+};
+struct EdgeCollapse {
+    IndexSet i1, i2;
+    float cost;
+    FVector newPos;
+
+    bool operator<(const EdgeCollapse& other) const {
+        return cost > other.cost; // 최소 힙: 낮은 cost 우선
+    }
+};
+
+
+class QEMSimplifier {
+public:
+    // targetVertexCount까지 단순화 (예제에서는 면 정보를 이용하여 인접 정점만 후보로 처리)
+    static void Simplify(FObjInfo& obj, int targetVertexCount) {
+        int numVertices = obj.Vertices.Num();
+        int numFaces = obj.VertexIndices.Num() / 3;
+
+        // 1. 각 정점에 대한 QEM 행렬 계산 (각 정점에 인접한 삼각형 면들로부터)
+        std::vector<Quadric> quadrics(numVertices);
+        for (int i = 0; i < numFaces; i++) {
+            int v0 = obj.VertexIndices[i * 3 + 0];
+            int v1 = obj.VertexIndices[i * 3 + 1];
+            int v2 = obj.VertexIndices[i * 3 + 2];
+            
+            FVector p0 = obj.Vertices[v0];
+            FVector p1 = obj.Vertices[v1];
+            FVector p2 = obj.Vertices[v2];
+            
+            FVector normal = (p1 - p0).Cross(p2 - p0).Normalize();
+            float d = -normal.Dot(p0);
+            
+            Quadric q;
+            q.data[0] = normal.x * normal.x;
+            q.data[1] = normal.x * normal.y;
+            q.data[2] = normal.x * normal.z;
+            q.data[3] = normal.x * d;
+            q.data[4] = normal.y * normal.y;
+            q.data[5] = normal.y * normal.z;
+            q.data[6] = normal.y * d;
+            q.data[7] = normal.z * normal.z;
+            q.data[8] = normal.z * d;
+            q.data[9] = d * d;
+            
+            quadrics[v0].Add(q);
+            quadrics[v1].Add(q);
+            quadrics[v2].Add(q);
+        }
+
+        // 2. 면 정보로부터 실제로 연결된 엣지 후보 목록을 생성합니다.
+        // 각 면의 엣지 (v0-v1, v1-v2, v2-v0)만 고려합니다.
+        std::set<std::pair<IndexSet, IndexSet>> edgeSet;
+        for (int i = 0; i < numFaces; i++) {
+            uint32 v[3] = { obj.VertexIndices[i * 3 + 0],
+            obj.VertexIndices[i * 3 + 1],
+            obj.VertexIndices[i * 3 + 2] };
+            uint32 n[3]= { obj.NormalIndices[i * 3 + 0],
+            obj.NormalIndices[i * 3 + 1],
+            obj.NormalIndices[i * 3 + 2] };
+            uint32 t[3] = {obj.TextureIndices[i * 3 + 0],
+            obj.TextureIndices[i * 3 + 1],
+            obj.TextureIndices[i * 3 + 2] };
+            // 정렬하여 (min, max) 쌍으로 저장
+            for (int j = 0; j < 3; j++) {
+                uint32 a = v[j], b = v[(j+1)%3];
+                uint32 c = n[j], d = n[(j+1)%3];
+                uint32 e = t[j], f = t[(j+1)%3];
+                if (a > b)
+                {
+                    std::swap(a, b);
+                    std::swap(c, d);
+                    std::swap(e, f);
+                }
+                IndexSet minEdge(a,c,e), maxEdge(b,e,f);
+                edgeSet.insert({minEdge, maxEdge});
+            }
+        }
+        
+        // 3. 후보 엣지들을 우선순위 큐에 추가 (병합 가능한지 검사)
+        std::priority_queue<EdgeCollapse> collapseQueue;
+        for (const auto& edge : edgeSet) {
+            IndexSet minEdge = edge.first, maxEdge = edge.second;
+
+            FVector midpoint = (obj.Vertices[minEdge.verIndex] + obj.Vertices[maxEdge.verIndex]) * 0.5f;
+            float cost = ComputeCollapseCost(quadrics[minEdge.verIndex], quadrics[maxEdge.verIndex], midpoint);
+            collapseQueue.push({minEdge, maxEdge, cost, midpoint});
+        }
+
+        // 4. 목표 정점 개수까지 단순화 수행
+        while (obj.Vertices.Num() > (size_t)targetVertexCount && !collapseQueue.empty()) {
+            EdgeCollapse bestCollapse = collapseQueue.top();
+            collapseQueue.pop();
+            uint32 v1 = bestCollapse.i1.verIndex,v2 = bestCollapse.i2.verIndex;
+            uint32 n1 = bestCollapse.i1.norIndex, n2 = bestCollapse.i2.norIndex;
+            uint32 t1 = bestCollapse.i1.texIndex, t2 = bestCollapse.i2.texIndex;
+            
+            // 병합 전 원래 v1과 v2의 위치와 UV 기록
+            FVector oldPos_v1 = obj.Vertices[v1];
+            FVector oldPos_v2 = obj.Vertices[v2];
+            FVector normal_v1 = obj.Normals[n1];
+            FVector normal_v2 = obj.Normals[n2];
+            FVector2D uv_v1 = obj.UVs[t1];
+            FVector2D uv_v2 = obj.UVs[t2];
+            
+            // obj.Vertices[v1] = (oldPos_v1 + oldPos_v2) * .5f;
+            // obj.Normals[n1] = (normal_v1 + normal_v2) * 0.5f;
+            // obj.Normals[n1] = obj.Normals[n1].Normalize();
+            //
+            //
+            // float totalDist = (oldPos_v2 - oldPos_v1).Magnitude();
+            // float movedDist = (bestCollapse.newPos - oldPos_v1).Magnitude();
+            // float t = (totalDist > 1e-6f) ? (movedDist / totalDist) : 0.5f;
+            // obj.UVs[t1] =  FVector2D::Lerp(uv_v1, uv_v2, t);
+            // obj.UVs[t1] =  uv_v1;
+
+            
+            // 정점 삭제: v2 정점을 제거 (삭제 후 인덱스 재매핑 필요)
+            obj.Vertices.RemoveAt(v2);
+            // obj.Normals.RemoveAt(n2);
+            // obj.UVs.RemoveAt(t2);
+            
+            // 인덱스 재매핑: 삭제된 정점 v2에 해당하는 인덱스는 v1로, v2보다 큰 인덱스는 1씩 감소
+            for (auto& index : obj.VertexIndices) {
+                if (index == v2) index = v1;
+                else if (index > v2) index--;
+            }
+            // for (auto& index : obj.NormalIndices) {
+            //     if (index == n2) index = n1;
+            //     else if (index > n2) index--;
+            // }
+            // for (auto& index : obj.TextureIndices) {
+            //     if (index == t2) index = t1;
+            //     else if (index > t2) index--;
+            // }
+
+
+            edgeSet.clear();
+            quadrics.clear();
+            collapseQueue = std::priority_queue<EdgeCollapse>();
+            std::set<std::pair<IndexSet, IndexSet>> edgeSet;
+            for (int i = 0; i < numFaces; i++) {
+                uint32 v[3] = { obj.VertexIndices[i * 3 + 0],
+                obj.VertexIndices[i * 3 + 1],
+                obj.VertexIndices[i * 3 + 2] };
+                uint32 n[3]= { obj.NormalIndices[i * 3 + 0],
+                obj.NormalIndices[i * 3 + 1],
+                obj.NormalIndices[i * 3 + 2] };
+                uint32 t[3] = {obj.TextureIndices[i * 3 + 0],
+                obj.TextureIndices[i * 3 + 1],
+                obj.TextureIndices[i * 3 + 2] };
+                if ((v[0] == v[1] || v[1] == v[2] || v[2] == v[0]))
+                {
+                    obj.VertexIndices.RemoveAt((i*3+2));
+                    obj.VertexIndices.RemoveAt(i*3+1);
+                    obj.VertexIndices.RemoveAt(i*3);
+                    obj.NormalIndices.RemoveAt((i*3+2));
+                    obj.NormalIndices.RemoveAt(i*3+1);
+                    obj.NormalIndices.RemoveAt(i*3);
+                    obj.TextureIndices.RemoveAt((i*3+2));
+                    obj.TextureIndices.RemoveAt(i*3+1);
+                    obj.TextureIndices.RemoveAt(i*3);
+     
+                    numFaces--;
+                    i--;
+                    continue;
+                }
+                // 정렬하여 (min, max) 쌍으로 저장
+                for (int j = 0; j < 3; j++) {
+                    uint32 a = v[j], b = v[(j+1)%3];
+                    uint32 c = n[j], d = n[(j+1)%3];
+                    uint32 e = t[j], f = t[(j+1)%3];
+                    if (a > b)
+                    {
+                        std::swap(a, b);
+                        std::swap(c, d);
+                        std::swap(e, f);
+                    }
+                    edgeSet.insert({{a,c,e}, {b,d,f}});
+                }
+            }
+            
+            numVertices = obj.Vertices.Num();
+            numFaces = obj.VertexIndices.Num() / 3;
+            quadrics.resize(numVertices);
+            for (int i = 0; i < numFaces; i++) {
+                int v0 = obj.VertexIndices[i * 3 + 0];
+                int v1 = obj.VertexIndices[i * 3 + 1];
+                int v2 = obj.VertexIndices[i * 3 + 2];
+                
+                FVector p0 = obj.Vertices[v0];
+                FVector p1 = obj.Vertices[v1];
+                FVector p2 = obj.Vertices[v2];
+                
+                FVector normal = (p1 - p0).Cross(p2 - p0).Normalize();
+                float d = -normal.Dot(p0);
+                
+                Quadric q;
+                q.data[0] = normal.x * normal.x;
+                q.data[1] = normal.x * normal.y;
+                q.data[2] = normal.x * normal.z;
+                q.data[3] = normal.x * d;
+                q.data[4] = normal.y * normal.y;
+                q.data[5] = normal.y * normal.z;
+                q.data[6] = normal.y * d;
+                q.data[7] = normal.z * normal.z;
+                q.data[8] = normal.z * d;
+                q.data[9] = d * d;
+                
+                quadrics[v0].Add(q);
+                quadrics[v1].Add(q);
+                quadrics[v2].Add(q);
+            }
+            for (const auto& edge : edgeSet) {
+                IndexSet minEdge = edge.first, maxEdge = edge.second;
+                FVector midpoint = (obj.Vertices[minEdge.verIndex] + obj.Vertices[maxEdge.verIndex]) * 0.5f;
+                float cost = ComputeCollapseCost(quadrics[minEdge.verIndex], quadrics[maxEdge.verIndex], midpoint);
+                collapseQueue.push({minEdge, maxEdge, cost, midpoint});
+            }
+        }
+    }
+
+private:
+    // 간단한 엣지 축소 비용 계산 함수
+    // 실제 구현에서는 newPos^T * q * newPos 를 계산하는 방식이 더 정확하지만, 여기서는 q.data[9]를 사용합니다.
+    static float ComputeCollapseCost(const Quadric& q1, const Quadric& q2, FVector& newPos) {
+        Quadric q = q1;
+        q.Add(q2);
+    
+        // 4D 벡터 확장: newPos = (x, y, z, 1)
+        float x = newPos.x;
+        float y = newPos.y;
+        float z = newPos.z;
+    
+        // newPos^T * Q * newPos 계산
+        float cost =
+            q.data[0] * x * x + 2.0f * q.data[1] * x * y + 2.0f * q.data[2] * x * z + 2.0f * q.data[3] * x +  
+            q.data[4] * y * y + 2.0f * q.data[5] * y * z + 2.0f * q.data[6] * y +
+            q.data[7] * z * z + 2.0f * q.data[8] * z +
+            q.data[9];  // d*d 항
+    
+        return cost;
+    }
+    static float ComputeCollapseCost(const Quadric& q1, const Quadric& q2, FVector& newPos, 
+                                   const FVector2D& uv1, const FVector2D& uv2, 
+                                   const FVector& normal1, const FVector& normal2) {
+        Quadric q = q1;
+        q.Add(q2);
+    
+        float x = newPos.x;
+        float y = newPos.y;
+        float z = newPos.z;
+    
+        // 기본 기하학적 비용 계산: newPos^T * Q * newPos
+        float geomCost =
+            q.data[0] * x * x + 2.0f * q.data[1] * x * y + 2.0f * q.data[2] * x * z + 2.0f * q.data[3] * x +
+            q.data[4] * y * y + 2.0f * q.data[5] * y * z + 2.0f * q.data[6] * y +
+            q.data[7] * z * z + 2.0f * q.data[8] * z +
+            q.data[9];
+    
+        // UV 차이에 따른 비용 (단순 예제: 두 UV 사이의 유클리드 거리의 제곱)
+        float du = uv1.x - uv2.x;
+        float dv = uv1.y - uv2.y;
+        float uvCost = du * du + dv * dv;
+    
+        // 노말 차이에 따른 비용 (두 노말의 코사인 유사도 기반, 1 - DotProduct)
+        float normalDiff = 1.0f - normal1.Dot(normal2);
+        float normalCost = normalDiff * normalDiff;
+    
+        // 가중치 인자: alpha와 beta는 조절 가능한 파라미터
+        const float alpha = .1f;  // UV 비용에 대한 가중치
+        const float beta = .1f;   // 노말 비용에 대한 가중치
+    
+        float totalCost = geomCost + alpha * uvCost + beta * normalCost;
+        return totalCost;
+    }
 };
